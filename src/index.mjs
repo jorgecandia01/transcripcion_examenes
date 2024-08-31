8135b90846162339b62001a1866a9842468f0c80
Jorge <jorgecandia2001@gmail.com>
2024-08-31 15:41:22 +0200

    multiples exámenes en paralelo, con hoja de respuestas en .png

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,10 @@
 import dotenv from 'dotenv';
 import OpenAI from "openai";
-import { zodResponseFormat } from "openai/helpers/zod";
-import fs from 'fs';
-import path from 'path';
 import * as xlsx from 'xlsx';
-import { getSchema } from './pdfUtils.mjs';
-import { transcripcionOCRImagen, convertPdfToImages } from './pdfUtils.mjs';
+import { transcripcionOCRImagen, obtenerArchivosPDF, loadAndConvertPdf, llamarGPT, asegurarParPDFPNG, convertirPNGABase64 } from './pdfUtils.mjs';
 import pino from 'pino';
+import path from 'path';
+
 
 dotenv.config();
 const logger = pino();
@@ -15,125 +13,106 @@ const logger = pino();
 const api_key = process.env.OPENAI_API_KEY;
 const openai = new OpenAI({ apiKey: api_key });
 
-let tokensI = 0;
-let tokensO = 0;
 const precioI = 2.5 / 1000000;
 const precioO = 10 / 1000000;
 
 
-let array_jsons_imagenes = await loadAndConvertPdf() || []; // Array de JSONs con imagen base64 y más cosas menos importantes
+const pdfs = obtenerArchivosPDF();
+logger.info(`Archivos PDF para transcribir: ${pdfs}`);
+
+// transcribirPdf('examen2.pdf');
+
+for(const pdf of pdfs) {
+    if(asegurarParPDFPNG(pdf)){
+        logger.info('Se empieza a transcribir el PDF ' + pdf);
+        // Sin el await para que no se interrumpa y se hagan múltiples PDFs a la vez (chatgpt tarda una eternidad)
+        transcribirPdf(pdf); // Mucho cuidado con los RATE LIMITS -> si son muchos PDFs puede saltar error
+    } else {
+        logger.info('NO se procede a transcribir el PDF, ' + pdf + 'se para al siguiente PDF');
+    }
+}
+
+logger.info('Transcripción de todos los PDFs terminada');
 
 
-// Realiza OCR a las imágenes
-logger.info('Se empieza con el OCR');
-const array_jsons_imagenesOCR = await Promise.all(
-    array_jsons_imagenes.map(async (imagen_json) => {
-        const ocr = await transcripcionOCRImagen([imagen_json.base64]);
-        console.log(imagen_json);
-        return { ...imagen_json, ocr };
-    })
-);
 
 
-// Crea un libro de trabajo (workbook) y una hoja de trabajo (worksheet)
-const workbook = xlsx.utils.book_new();
-const worksheet = xlsx.utils.aoa_to_sheet([['Índice', 'Enunciado', 'Respuesta A', 'Respuesta B', 'Respuesta C', 'Respuesta D', 'Respuesta Correcta', 'Justificación']]);
 
 
-logger.info('Se empieza a llamar a chatgpt');
-for (const imagen_json of array_jsons_imagenesOCR) {
-    logger.info(`Página ${imagen_json.page} de ${array_jsons_imagenesOCR.length}`);
-    try {
-        const respuesta = await llamarGPT(openai, imagen_json);
-        const contenido = JSON.parse(respuesta.choices[0].message.content); 
 
-        tokensI += respuesta.usage.prompt_tokens;
-        tokensO += respuesta.usage.completion_tokens;
+/// Función core que toma como argumento el nombre del archivo PDF y crea un .xlsx con la transcripción del examen
+async function transcribirPdf(nombre) {
+    // Extraigo el nombre base para utilizarlo en un futuro
+    const nombreBase = path.basename(nombre, '.pdf');
 
-        if (Array.isArray(contenido.array_preguntas)) {
-            contenido.array_preguntas.forEach(pregunta => {
-                xlsx.utils.sheet_add_aoa(worksheet, [[
-                    pregunta.indice,
-                    pregunta.enunciado,
-                    pregunta.respuesta_a,
-                    pregunta.respuesta_b,
-                    pregunta.respuesta_c,
-                    pregunta.respuesta_d,
-                    pregunta.respuesta_correcta,
-                    pregunta.respuesta_justificacion
-                ]], { origin: -1 });
-                logger.info('Pregunta añadida correctamente');
-            });
-        } else {
-            logger.warn('Array nulo -> no hay preguntas, no se añade nada');
+    // Objengo la imagen png en base64 para ingestarla a chatgpt
+    const imagen_respuestas = convertirPNGABase64(`${nombreBase}.png`);
+
+    // Cargar y convertir el PDF a imágenes
+    let array_jsons_imagenes = await loadAndConvertPdf(nombre) || []; // Array de JSONs con imagen base64 y más cosas menos importantes
+
+    // Variables para llevar la cuenta de los tokens
+    let tokensI = 0;
+    let tokensO = 0;
+
+    // Realiza OCR a las imágenes
+    logger.info('Se empieza con el OCR para ' + nombre);
+    const array_jsons_imagenesOCR = await Promise.all(
+        array_jsons_imagenes.map(async (imagen_json) => {
+            const ocr = await transcripcionOCRImagen([imagen_json.base64]);
+            // console.log(imagen_json);
+            return { ...imagen_json, ocr };
+        })
+    );
+
+    // Crea un libro de trabajo (workbook) y una hoja de trabajo (worksheet)
+    const workbook = xlsx.utils.book_new();
+    const worksheet = xlsx.utils.aoa_to_sheet([['Índice', 'Enunciado', 'Respuesta A', 'Respuesta B', 'Respuesta C', 'Respuesta D', 'Respuesta Correcta', 'Justificación']]);
+
+    // Llamada a GPT y procesamiento de las respuestas
+    logger.info('Se empieza a llamar a chatgpt para ' + nombre);
+    for (const imagen_json of array_jsons_imagenesOCR) {
+        try {
+            const respuesta = await llamarGPT(openai, imagen_json, imagen_respuestas);
+            const contenido = JSON.parse(respuesta.choices[0].message.content); 
+
+            tokensI += respuesta.usage.prompt_tokens;
+            tokensO += respuesta.usage.completion_tokens;
+
+            logger.info(`Se ha transcrito la página ${imagen_json.page} de ${array_jsons_imagenesOCR.length} para el PDF ${nombre}. Se han utilizado ${respuesta.usage.prompt_tokens + respuesta.usage.completion_tokens} tokens`);
+
+            if (Array.isArray(contenido.array_preguntas)) {
+                contenido.array_preguntas.forEach(pregunta => {
+                    xlsx.utils.sheet_add_aoa(worksheet, [[
+                        pregunta.indice,
+                        pregunta.enunciado,
+                        pregunta.respuesta_a,
+                        pregunta.respuesta_b,
+                        pregunta.respuesta_c,
+                        pregunta.respuesta_d,
+                        pregunta.respuesta_correcta,
+                        pregunta.respuesta_justificacion
+                    ]], { origin: -1 });
+                    logger.info('Pregunta añadida correctamente de ' + nombre);
+                });
+            } else {
+                logger.warn('Array nulo -> no hay preguntas, no se añade nada de ' + nombre);
+            }
+        } catch (error) {
+            logger.error('Error al llamar a GPT o procesar la respuesta en ' + nombre);
+            console.log(error);
+            continue; // Saltar a la siguiente iteración en caso de error
         }
-    } catch (error) {
-        logger.error('Error al llamar a GPT o procesar la respuesta:', error);
-        console.log(error);
-        continue; // Saltar a la siguiente iteración en caso de error
     }
+
+    // Añadir la hoja de trabajo al libro de trabajo y guardar el archivo Excel
+    xlsx.utils.book_append_sheet(workbook, worksheet, 'Preguntas');
+    xlsx.writeFile(workbook, `src/target/${nombreBase}.xlsx`);
+    logger.info('-- Preguntas guardadas en ' + `src/target/${nombreBase}.xlsx`);
+
+    // Calcular el costo
+    logger.info(`Precios para ${nombre}: Precio input: ${precioI * tokensI}, Precio output: ${precioO * tokensO}, Precio total: ${precioI * tokensI + precioO * tokensO}`);
 }
 
-// Añadir la hoja de trabajo al libro de trabajo y guardar el archivo Excel
-xlsx.utils.book_append_sheet(workbook, worksheet, 'Preguntas');
-xlsx.writeFile(workbook, 'examen_preguntas.xlsx');
-
-// Calcular el costo
-logger.info(`Precio input: ${precioI * tokensI}, Precio output: ${precioO * tokensO}, Precio total: ${precioI * tokensI + precioO * tokensO}`);
-
-async function llamarGPT(openai, imagen_json) {
-    const prompt_user = `Aquí está el OCR de la imagen: ${imagen_json.ocr}`;
-    const prompt_system = `Se pretende transformar un examen tipo test en formato PDF en un examen tipo test en formato JSON (JSON Schema).
-    A continuación, se te pasa una de las páginas del PDF convertida a imagen png base64 y una transcripción OCR básica. Debes siempre
-    fiarte más de tus capacidades de visión que de la transcripción OCR, ya que este es solamente una ayuda. El examen es un examen real
-    que tiene que pasar una persona para aprobar una oposición sanitaria en España.
-
-    Tu respuesta a este prompt debe ser un json que se ajusta al proporcionado por el JSON Schema. Este es un array de JSON en el que 
-    cada elemento del array representa una de las preguntas del examen tipo test que aparece en la imagen. Para las claves 'indice' 
-    (número de pregunta), 'enunciado', 
-    'respuesta_a', 'respuesta_b', 'respuesta_c', 'respuesta_d', debes simplemente transcribir al pie de la letra cada pregunta, es muy 
-    importante que no te inventes nada. Para las claves 'respuesta_correcta' y 'respuesta_justificacion', debes razonar cuál es la respuesta
-    correcta y la justificación de por qué has elegido esa opción. Es decir, aquí sí que debes ser un experto, saber qué es correcto y aportar
-    con tu conocimiento. La clave 'error' será false por defecto, y está reservada para los casos en los que la imagen proporcionada no contiene
-    preguntas, o estas no cumplen con el JSON Schema proporcionado. Si pasa esto o cualquier otro problema, su valor debe ser 'true'.
-
-    Como se itera sobre todas las páginas del PDF, es posible que la imagen proporcionada no contenga preguntas, dado que al principio y al final
-    del examen suele haber una introducción y una despedida, y pueden existir páginas en blanco que no contienen preguntas. Para estos casos,
-    hemos definido el array como z.array(PreguntaSchema).nullable(). Por lo tanto, si la imagen proporcionada no contiene preguntas, el array 
-    debe ser null.`;
-
-    const ExamenSchema = getSchema();
-
-    const completion = await openai.chat.completions.create({
-        model: "gpt-4o-2024-08-06",
-        temperature: 0,
-        messages: [
-            { role: "system", content: prompt_system },
-            { role: "user", content: [
-                {type: "text", text: prompt_user},
-                {type: "image_url", image_url: {url: `data:image/png;base64,${imagen_json.base64}`, detail: "high"}},
-            ] },
-        ],
-        response_format: zodResponseFormat(ExamenSchema, 'examen_schema'),
-    });
-
-    // console.log(completion.choices[0].message.content);
-    return completion;
-}
-
-// Función para cargar y convertir el PDF
-async function loadAndConvertPdf() {
-    try {
-        const __dirname = path.dirname(new URL(import.meta.url).pathname);
-        const pdfPath = path.join(__dirname, 'pdfs', 'examen2.pdf');
-        const pdfBuffer = fs.readFileSync(pdfPath);
-        const pdfBase64 = pdfBuffer.toString('base64');
-        const imagenes = await convertPdfToImages(pdfBase64);
-
-        return imagenes;
-    } catch (error) {
-        logger.error("Error al cargar y convertir el PDF: ", error);
-    }
-}
 
 
--- src/pdfUtils.mjs
+++ src/pdfUtils.mjs
@@ -3,9 +3,10 @@ import Tesseract from 'tesseract.js';
 import { z } from 'zod';
 import fs from 'fs';
 import path from 'path';
+import { zodResponseFormat } from "openai/helpers/zod";
 
-const archivosPDF = obtenerArchivosPDF();
-console.log(archivosPDF);
+// const archivosPDF = obtenerArchivosPDF();
+// console.log(archivosPDF);
 
 export async function transcripcionOCRImagen(imagesBase64) {
     const transcriptions = await Promise.all(imagesBase64.map(async (base64Image) => {
@@ -58,7 +59,7 @@ export function getSchema() {
 
 
 
-function obtenerArchivosPDF() {
+export function obtenerArchivosPDF() {
     const targetPath = path.join(process.cwd(), 'src/target');
     const archivosPDF = [];
 
@@ -74,3 +75,113 @@ function obtenerArchivosPDF() {
 
     return archivosPDF;
 }
+
+
+
+
+export async function llamarGPT(openai, imagen_json, imagen_respuestas) {
+    const prompt_user_examen = `Aquí está la imagen de una página del examen junto al OCR de la imagen: ${imagen_json.ocr}`;
+    const prompt_user_respuestas = `Aquí está la imagen que contiene todas las respuestas del examen.`;
+    const prompt_system = `Se pretende transformar un examen tipo test en formato PDF en un examen tipo test en formato JSON (JSON Schema).
+    A continuación, se te pasa una de las páginas del PDF convertida a imagen png base64 y una transcripción OCR básica. Debes siempre
+    fiarte más de tus capacidades de visión que de la transcripción OCR, ya que este es solamente una ayuda. El examen es un examen real
+    que tiene que pasar una persona para aprobar una oposición sanitaria en España.
+
+    Tu respuesta a este prompt debe ser un json que se ajusta al proporcionado por el JSON Schema. Este es un array de JSON en el que 
+    cada elemento del array representa una de las preguntas del examen tipo test que aparece en la imagen. Para las claves 'indice' 
+    (número de pregunta), 'enunciado', 'respuesta_a', 'respuesta_b', 'respuesta_c', 'respuesta_d', debes simplemente transcribir al pie de
+    la letra cada pregunta, es muy importante que no te inventes nada. 
+    Para la clave 'respuesta_correcta', debes seleccionar la respuesta correcta. Esta se encuentra dentro de la segunda imagen que te paso, 
+    que contiene todas las respuestas correctas para todo el examen, por lo que debes escoger con cuidado la respuesta correcta fijándote en
+    los índices de las preguntas de la primera imagen y su correspondencia en la hoja de soluciones, la segunda imagen.
+    Para la clave 'respuesta_justificacion', debes razonar la pregunta y justificar la respuesta como un 
+    experto en la materia que eres. Es decir, aquí sí que debes ser un experto, saber por qué es correcto y aportar con tu conocimiento. 
+    La clave 'error' será false por defecto, y está reservada para los casos en los que la imagen proporcionada no contiene
+    preguntas, o estas no cumplen con el JSON Schema proporcionado. Si pasa esto o cualquier otro problema, su valor debe ser 'true'.
+
+    Como se itera sobre todas las páginas del PDF, es posible que la imagen proporcionada no contenga preguntas, dado que al principio y al final
+    del examen suele haber una introducción y una despedida, y pueden existir páginas en blanco que no contienen preguntas. Para estos casos,
+    hemos definido el array como z.array(PreguntaSchema).nullable(). Por lo tanto, si la imagen proporcionada no contiene preguntas, el array 
+    debe ser null.`;
+
+    const ExamenSchema = getSchema();
+
+    const completion = await openai.chat.completions.create({
+        model: "gpt-4o-2024-08-06",
+        temperature: 0,
+        messages: [
+            { role: "system", content: prompt_system },
+            { role: "user", content: [
+                {type: "text", text: prompt_user_examen},
+                {type: "image_url", image_url: {url: `data:image/png;base64,${imagen_json.base64}`, detail: "high"}},
+            ] },
+            { role: "user", content: [
+                {type: "text", text: prompt_user_respuestas},
+                {type: "image_url", image_url: {url: `data:image/png;base64,${imagen_respuestas}`, detail: "high"}},
+            ] },
+        ],
+        response_format: zodResponseFormat(ExamenSchema, 'examen_schema'),
+    });
+
+    return completion;
+}
+
+
+/// Función para cargar y convertir el PDF a imágenes (array de JSON que contienen la clave 'base64' de la imagen png)
+export async function loadAndConvertPdf(nombre) {
+    try {
+        const __dirname = path.dirname(new URL(import.meta.url).pathname);
+        const pdfPath = path.join(__dirname, 'target', nombre);
+        const pdfBuffer = fs.readFileSync(pdfPath);
+        const pdfBase64 = pdfBuffer.toString('base64');
+        const imagenes = await convertPdfToImages(pdfBase64);
+
+        return imagenes;
+    } catch (error) {
+        logger.error("Error al cargar y convertir el PDF: ", error);
+    }
+}
+
+
+
+export function asegurarParPDFPNG(nombrePDF) {
+    const targetDir = path.join(process.cwd(), 'src', 'target');
+    const archivoPDF = path.join(targetDir, nombrePDF);
+
+    // Verificar si el archivo .pdf existe
+    if (!fs.existsSync(archivoPDF)) {
+        console.log(`El archivo PDF ${nombrePDF} no existe en ${targetDir}.`);
+        return false;
+    }
+
+    // Obtener el nombre base sin la extensión
+    const nombreBase = path.basename(nombrePDF, '.pdf');
+    const archivoPNG = path.join(targetDir, `${nombreBase}.png`);
+
+    // Verificar si el archivo .png correspondiente existe
+    if (!fs.existsSync(archivoPNG)) {
+        console.log(`El archivo PNG correspondiente para ${nombrePDF} NO existe`);
+        return false;
+    } else {
+        console.log(`El archivo PNG correspondiente para ${nombrePDF} existe: ${archivoPNG}`);
+        return true;
+    }
+}
+
+
+
+export function convertirPNGABase64(nombrePNG) {
+    const targetDir = path.join(process.cwd(), 'src', 'target');
+    const archivoPNG = path.join(targetDir, nombrePNG);
+
+    // Leer el archivo .png y convertirlo a base64
+    try {
+        const imagenBuffer = fs.readFileSync(archivoPNG);
+        const imagenBase64 = imagenBuffer.toString('base64');
+        return imagenBase64;
+    } catch (error) {
+        console.error(`Error al leer el archivo PNG ${nombrePNG}:`, error);
+        return null;
+    }
+}
+

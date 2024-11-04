984a54e4fb73f337fe6ca5f58c8c996411e40ba4
Jorge <jorgecandia2001@gmail.com>
2024-11-04 02:03:51 +0100

    cambio a solo transcripcion

--- src/pdfUtils.mjs
+++ src/pdfUtils.mjs
@@ -67,8 +67,8 @@ export function getSchema5() {
         respuesta_c: z.string(),
         respuesta_d: z.string(),
         respuesta_e: z.string(),
-        respuesta_correcta: z.enum(['A', 'B', 'C', 'D', 'E', 'ANULADA', '']),
-        respuesta_justificacion: z.string(),
+        // respuesta_correcta: z.enum(['A', 'B', 'C', 'D', 'E', 'ANULADA', '']),
+        // respuesta_justificacion: z.string(),
         error: z.boolean(),
     });
 
@@ -177,6 +177,24 @@ export async function llamarGPT(openai, imagen_json, imagen_respuestas) {
 }
 
 
+
+// Para la clave 'respuesta_correcta', debes seleccionar la respuesta correcta. Esta se encuentra dentro de la segunda imagen que te paso, 
+//     que contiene todas las respuestas correctas para todo el examen, por lo que debes escoger con cuidado la respuesta correcta fijándote en
+//     los índices de las preguntas de la primera imagen y su correspondencia en la hoja de soluciones, que es la segunda imagen. Algunas veces, la
+//     respuesta correcta aparece vacía, entonces pondrás 'ANULADA'. NUNCA te inventarás la respuesta correcta. También pondrás un string 
+//     vacío en 'respuesta_correcta' en caso de que no logres identificar con precisión la respuesta correcta en la imagen, pero NO TE LA INVENTARÁS.
+//     Para la clave 'respuesta_justificacion', debes razonar la pregunta y justificar la respuesta como un experto en la materia que eres,
+//     pero SIEMPRE SIEMPRE fijándote en la respuesta marcada como correcta en la imagen proporcionada en el prompt y justificando por qué la respuesta
+//     seleccionada es la correcta. Es decir, PRIMERO debes asociar la respuesta correcta desde la imagen y meterla en 'respuesta_correcta', y 
+//     es luego y SÓLAMENTE luego, cuando debes justificar por qué esa respuesta es la correcta. 
+//     Es decir, aquí sí que debes ser un experto, saber por qué es correcto y aportar con tu conocimiento, en un rango aproximado de 100-150 palabras. 
+
+//     Voy a repetirlo una vez más porque es muy importante: A LA HORA DE ESCOGER RESPUESTA CORRECTA, DEBES ESCOGER LA OPCIÓN CORRESPONDIENTE
+//     DE LA IMAGEN QUE MUESTRA TODAS LAS RESPUESTAS. TIENES PROHIBIDO INVENTARTE CUÁL ES LA RESPUESTA CORRECTA PARA ELIMINAR EL RIEGO DE
+//     EQUIVOCACIÓN, YA TE HEMOS PREPARADO NOSOTROS LA RESPUESTA CORRECTA EN LA OTRA IMAGEN, SOLO TIENES QUE MARCARLA FIJÁNDOTE EN QUÉ ÍNDICE
+//     TIENE LA PREGUNTA QUE ESTÁS TRANSCRIBIENDO Y EL ÍNDICE DE LA PREGUNTA EN LA IMAGEN DE RESPUESTAS CORRECTAS. 
+//     POR FAVOR, NO TE CONFUNDAS DE RESPUESTA CORRECTA, FÍJATE BIEN EN LOS ÍNDICES EN LAS DOS IMÁGENES.
+    
 /// Misma función para llamar a chatgpt pero aqui a veces hay una respuesta más (e) (tambien meto lo de respuesta vacía)
 export async function llamarGPT5(openai, imagen_json, imagen_respuestas) {
     const prompt_user_examen = `Aquí está la imagen de una página del examen junto al OCR de la imagen: ${imagen_json.ocr}`;
@@ -192,24 +210,9 @@ export async function llamarGPT5(openai, imagen_json, imagen_respuestas) {
     la letra cada pregunta, es muy importante que no te inventes nada. Hay veces que además de las 4 respuestas, aparecerá una quinta respuesta,
     la respuesta e, que deberás transcribir en la clave 'respuesta_e'. Cuando no exista esta quinta respuesta (o cualquier otra), 
     simplemente devolverás un string vacío ('').
-    Para la clave 'respuesta_correcta', debes seleccionar la respuesta correcta. Esta se encuentra dentro de la segunda imagen que te paso, 
-    que contiene todas las respuestas correctas para todo el examen, por lo que debes escoger con cuidado la respuesta correcta fijándote en
-    los índices de las preguntas de la primera imagen y su correspondencia en la hoja de soluciones, que es la segunda imagen. Algunas veces, la
-    respuesta correcta aparece vacía, entonces pondrás 'ANULADA'. NUNCA te inventarás la respuesta correcta. También pondrás un string 
-    vacío en 'respuesta_correcta' en caso de que no logres identificar con precisión la respuesta correcta en la imagen, pero NO TE LA INVENTARÁS.
-    Para la clave 'respuesta_justificacion', debes razonar la pregunta y justificar la respuesta como un experto en la materia que eres,
-    pero SIEMPRE SIEMPRE fijándote en la respuesta marcada como correcta en la imagen proporcionada en el prompt y justificando por qué la respuesta
-    seleccionada es la correcta. Es decir, PRIMERO debes asociar la respuesta correcta desde la imagen y meterla en 'respuesta_correcta', y 
-    es luego y SÓLAMENTE luego, cuando debes justificar por qué esa respuesta es la correcta. 
-    Es decir, aquí sí que debes ser un experto, saber por qué es correcto y aportar con tu conocimiento, en un rango aproximado de 100-150 palabras. 
+    
     La clave 'error' será false por defecto, y está reservada para los casos en los que la imagen proporcionada no contiene
     preguntas, o estas no cumplen con el JSON Schema proporcionado. Si pasa esto o cualquier otro problema, su valor debe ser 'true'.
-
-    Voy a repetirlo una vez más porque es muy importante: A LA HORA DE ESCOGER RESPUESTA CORRECTA, DEBES ESCOGER LA OPCIÓN CORRESPONDIENTE
-    DE LA IMAGEN QUE MUESTRA TODAS LAS RESPUESTAS. TIENES PROHIBIDO INVENTARTE CUÁL ES LA RESPUESTA CORRECTA PARA ELIMINAR EL RIEGO DE
-    EQUIVOCACIÓN, YA TE HEMOS PREPARADO NOSOTROS LA RESPUESTA CORRECTA EN LA OTRA IMAGEN, SOLO TIENES QUE MARCARLA FIJÁNDOTE EN QUÉ ÍNDICE
-    TIENE LA PREGUNTA QUE ESTÁS TRANSCRIBIENDO Y EL ÍNDICE DE LA PREGUNTA EN LA IMAGEN DE RESPUESTAS CORRECTAS. 
-    POR FAVOR, NO TE CONFUNDAS DE RESPUESTA CORRECTA, FÍJATE BIEN EN LOS ÍNDICES EN LAS DOS IMÁGENES.
 
     Como se itera sobre todas las páginas del PDF, es posible que la imagen proporcionada no contenga preguntas, dado que al principio y al final
     del examen suele haber una introducción y una despedida, y pueden existir páginas en blanco que no contienen preguntas. Para estos casos,
@@ -227,10 +230,10 @@ export async function llamarGPT5(openai, imagen_json, imagen_respuestas) {
                 {type: "text", text: prompt_user_examen},
                 {type: "image_url", image_url: {url: `data:image/png;base64,${imagen_json.base64}`, detail: "high"}},
             ] },
-            { role: "user", content: [
-                {type: "text", text: prompt_user_respuestas},
-                {type: "image_url", image_url: {url: `data:image/png;base64,${imagen_respuestas}`, detail: "high"}},
-            ] },
+            // { role: "user", content: [
+            //     {type: "text", text: prompt_user_respuestas},
+            //     {type: "image_url", image_url: {url: `data:image/png;base64,${imagen_respuestas}`, detail: "high"}},
+            // ] },
         ],
         response_format: zodResponseFormat(ExamenSchema5, 'examen_schema5'),
     });
